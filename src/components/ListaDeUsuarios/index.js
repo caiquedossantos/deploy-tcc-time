2cd84eea4785028ecfc49e770bfc2c33ab555d88
caiquedossantos <151476904+caiquedossantos@users.noreply.github.com>
2025-05-23 21:22:40 -0300

    Add files via upload

---
+++ src/components/ListaDeUsuarios/index.js
@@ -0,0 +1,44 @@
+// src\components\ListaDeUsuarios\index.js
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+import './styles.css'
+
+function ListaDeUsuarios() {
+    const [usuarios, setUsuarios] = useState([])
+
+    useEffect(() => {
+        const carregarUsuarios = async () => {
+            try {
+                const response = await axios.get('https://deploy-tcc-time.onrender.com/Elenco')
+                setUsuarios(response.data)
+            } catch (error) {
+                alert('Erro ao buscar os jogadores: ', error)
+                setUsuarios([])
+            }
+        }
+        carregarUsuarios()
+    }, [])
+
+    return (
+        <ul id="listaUsurios" className="lista-usuarios">
+            {usuarios.length === 0 ? (
+                <li>Nenhum jogador encontrado.</li>
+            ) : (
+                usuarios.map( usuario => (
+                    <li key={usuario.id}>
+                        <strong>Nome: </strong> {usuario.nome}<br />
+                        <strong>Sexo: </strong> {usuario.sexo}<br />
+                        <strong>Idade: </strong> {usuario.idade}<br />
+                        <strong>Altura: </strong> {usuario.altura}<br />
+                        <strong>Peso: </strong> {usuario.peso}<br />
+                        <strong>Número da Camisa: </strong> {usuario.numero}<br />        
+                    </li>
+                ))
+            )}
+        </ul>
+    )
+    
+}
+
+export default ListaDeUsuarios
